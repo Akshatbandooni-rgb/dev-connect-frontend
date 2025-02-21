685634c5b95bbae90c59cf7fd34c55989707ef4a
Akshat Bandooni <63807929+Akshatbandooni-rgb@users.noreply.github.com>
2025-02-21 21:10:26 +0530

    Created Login page and basic event handling setup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,46 @@
-const Login = () => {
-  return <div>Login</div>;
-};
+import { useState } from "react";
+import "./styles/Login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    console.log(email, password);
+  };
+
+  return (
+    <div className="container">
+      <div className="form-box">
+        <h2 className="title">Login</h2>
+        <div className="field">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="input"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="field">
+          <label className="label">Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            className="input"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className="button" onClick={login}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default Login;
